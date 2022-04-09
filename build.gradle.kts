496727bc5a149d081981b419ed5d060a4215ac6a
janghyoseog <bestdevhyo1225@gmail.com>
2022-04-09 17:41:03 +0900

    chore: Spring 프로젝트 설정

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.6.6"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.10"
+	kotlin("plugin.spring") version "1.6.10"
+	kotlin("plugin.jpa") version "1.6.10"
+	kotlin("plugin.allopen") version "1.6.10"
+	kotlin("kapt") version "1.6.10"
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+}
+
+group = "com.hs"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-aop:2.6.5")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.5")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis:2.6.5")
+	implementation("org.springframework.boot:spring-boot-starter-validation:2.6.5")
+	implementation("org.springframework.boot:spring-boot-starter-web:2.6.5")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.redisson:redisson:3.17.0")
+	runtimeOnly("com.h2database:h2:2.1.210")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.5")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
